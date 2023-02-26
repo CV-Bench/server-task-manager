9a7aa52d3e778557f1a2c91af89cb5346486bb1a
lorenz <lorenz.hufe@web.de>
2023-02-26 10:48:09 +0100

    current user setup - WIP app.py/ipynb

---
+++ app.py
@@ -0,0 +1,14 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/generate")
+# def generate_blender():
+
+
+# @app.route("/train")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+import requests
+
